f8f83c4c22f09c9b1a63f8cf61ec2ba64cac4cdf
kevorkkeheian <kevorkkeheian@gmail.com>
2022-04-03 19:24:14 +0300

    seo: add meta keywords and description

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,9 @@ export default function Home() {
       <Head>
         <title>Next Earth Promo</title>
         <meta name="description" content="Next Earth Promo Code"/>
+        <meta name="keywords" content="nextearth., nextearth.io, coupon codes, deals, discount, promo code>"></meta>
+        <meta name="description" content="Use this promo code to get a discount starting 5% and more, the more you use, the more you get discounts & remember... don't buy a single tile without a promo code, so that  you can  benefit!"></meta>
+        
         <link rel="icon" href="/favicon.ico" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
